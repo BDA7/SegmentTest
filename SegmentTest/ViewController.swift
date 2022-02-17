06b629cc37d01e4a26a286addcf913425b3fb386
d.bondarenko <d.bondarenko@okko.tv>
2022-02-17 16:34:23 +0300

    Initial Commit

---
+++ SegmentTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentTest
+//
+//  Created by d.bondarenko on 17.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
